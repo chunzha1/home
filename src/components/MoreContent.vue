6c5aab4e92070eed101bbf44fa8b448a73547211
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-12 17:52:11 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">I can write something here.</div>
+  <div class="more-content">I can write something I want here.</div>
 </template>
 
 <style lang="scss" scoped>
@@ -11,7 +11,7 @@
   width: 100%;
   height: 100%;
   font-family: "Pacifico-Regular";
-  font-size: 16px; // 可选：设置字体大小
+  font-size: 24px; // 可选：设置字体大小
   font-weight: normal; // 可选：设置字体粗细
 }
 </style>
