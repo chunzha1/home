6deeb23f05261dc6a7e4ec55a49e6880287ae71c
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-15 16:10:51 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -44,7 +44,7 @@ export const getPlayerList = async (server, type, id) => {
  * 一言
  */
 
-获取一言数据
+// 获取一言数据
 export const getHitokoto = async () => {
   const res = await fetch("https://v1.hitokoto.cn");
   return await res.json();
