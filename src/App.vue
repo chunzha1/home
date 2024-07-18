44814e0f89aa6cc8a81f3f14e61f287f2fe8e156
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-18 16:35:56 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -279,8 +279,8 @@ onBeforeUnmount(() => {
   }
   .floating-button {
   position: fixed;
-  bottom: calc(3% + 14px); // 距离底部5%加20px
-  right: calc(5% + 20px); // 距离右侧5%加20px
+  bottom: 10px; // 固定距离底部20px
+  right: 20px; // 固定距离右侧20px
   padding: 10px 20px;
   background-color: rgba(0, 0, 0, 0.6); // 半透明黑色背景
   color: white;
