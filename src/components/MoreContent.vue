f0e66b26b21fccc052d4b20e49eeeac149c2da78
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-24 10:50:04 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,23 +1,39 @@
 <template>
-  <div class="more-content">Aber Freund! wir kommen zu spät. Zwar leben die Götter,<br>
-    Aber über dem Haupt droben in anderer Welt.<br>
-    Weiß ich nicht und wozu Dichter in dürftiger Zeit?<br>
-  </div>
-  <div style="text-align: right; white-space: nowrap;">
-    <strong>Friedrich Hölderlin —— Brot und Wein 7</strong>
+  <div class="more-content">
+    <div class="poem-text">
+      Aber Freund! wir kommen zu spät. Zwar leben die Götter,<br>
+      Aber über dem Haupt droben in anderer Welt.<br>
+      Weiß ich nicht und wozu Dichter in dürftiger Zeit?
+    </div>
+    <div class="author-info">
+      <p>Friedrich Hölderlin —— Brot und<br>Wein 7</p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .more-content {
   display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 12px;
+  justify-content: space-between;
+  align-items: flex-end;
   width: 100%;
-  height: 100%;
   font-family: "Pacifico-Regular";
-  font-size: 14px; // 可选：设置字体大小
-  font-weight: normal; // 可选：设置字体粗细
+  font-size: 14px;
+  font-weight: normal;
+  margin-top: 12px;
+}
+
+.poem-text {
+  flex: 1;
+}
+
+.author-info {
+  text-align: right;
+  white-space: nowrap;
+  
+  p {
+    font-weight: bold;
+    margin: 0;
+  }
 }
 </style>
