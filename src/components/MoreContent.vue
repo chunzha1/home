c1959744ef3676069f64715f38eff8b81df4e9b5
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-24 10:31:23 +0800

    Update MoreContent.vue

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="more-content">Aber Freund! wir kommen zu spät. Zwar leben die Götter,<br>Aber über dem Haupt droben in anderer Welt.
     <br>Weiß ich nicht und wozu Dichter in dürftiger Zeit?<br>
-    <div style="text-align: right;">Friedrich Hölderlin —— Brot und Wein 7</div>
-Friedrich Hölderlin —— Brot und Wein 7</div>
+    <style="text-align: right;">Friedrich Hölderlin —— Brot und Wein 7</>
+</div>
 </template>
 
 <style lang="scss" scoped>
