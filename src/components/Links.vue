468e85ed2cb8e91cd611348d62d66fdc04f764b2
chunzha <125763492+chunzha1@users.noreply.github.com>
2024-07-24 16:44:07 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode,FileContract,EnvelopeOpenText,Paste, Burn, Download, Highlighter, Magic, Marker, NetworkWired } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, CommentDotsRegular, Cloud, Compass, Book, Fire, LaptopCode,FileContract,EnvelopeOpenText,Paste, Burn, Download, Highlighter, Magic, Marker, NetworkWired } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -79,6 +79,7 @@ const siteIcon = {
   Magic, 
   Marker,
   NetworkWired,
+  CommentDotsRegular,
 };
 
 // 链接跳转
